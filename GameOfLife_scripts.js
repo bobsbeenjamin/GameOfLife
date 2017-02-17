dc9314edba4369a179e8521f5855e421671fa884
Ben Clifford <ben.w.clifford@gmail.com>
2017-02-16 19:55:51 -0800

    Code re-org, using new knowledge (fixed screen edges)
    - Many variables were effectively being used as globals, so I reorganized the code base to reflect this (no functions use parameters now).
    - I fixed the issue where the right and bottom edges were not drawing correctly. There is now a 5-cell pad on each side (ie, there are 5 cells to the left, right, top, and bottom of the canvas).
    - The music now pauses with the pause button.
    - When the user presses the space bar or clicks the canvas, the play or pause button is activated (new global isPaused facilitates this).

--- GameOfLife_scripts.js
+++ GameOfLife_scripts.js
@@ -1,42 +1,55 @@
 /*
 TODOs:
-	User input
-		"Draw" seed
-		*Randomize seed
-			Sparsity of live cells
-		*Control speed
-		*Reuse buttons (Step)
+	UX
 		Improve overall L&F
-	Output
-		*Generation counter
+	Controls
+		Allow user to "draw" seed
+		Randomize seed
+			User-defined sparsity of live cells
 		Save board states to file
 			Read them back in
-		Play background music
 	Code org
 		Create additional var for boardDim
-		*Comment all functions
 	Misc
 		Detect when stable state has been reached
 		Explore Game variations 
 		*Handle edge of board gracefully
+Done:
+	UX
+		Play background music
+			Pause music when game is paused
+		Play/Pause by space bar
+		Play/Pause by screen click
+	Controls
+		Randomize seed
+		Control speed
+		Reuse buttons (Step)
+		Generation counter
+	Code org
+		Comment all functions
 */
 
 // Globals 
-boardDim = 100;
-pixelsPerSide = 5;
-generation = 0;
-speed = 80;
-game = null;
+var gameBoard = null;
+var boardDim = 110; // leave a padding of 5 on each side
+var pixelsPerSide = 5;
+var drawSpace = null;
+var audio = null;
+var generation = 0;
+var speed = 80;
+var delay = 5;
+var isPaused = false;
+var game = null;
 // The initial pattern (seed)
 // (Uncomment the seed that you want to test)
-seed = [[50,49], [50,50], [50,51]]; // "Blinker"
+//seed = [[50,49], [50,50], [50,51]]; // "Blinker"
 //seed = [[3,1], [3,2], [3,3], [2,2], [2,3], [2,4]]; // "Toad"
 //seed = [[51,52], [50,51], [50,50], [51,50], [52,50]]; // "Glider"
 //seed = [[25,26], [26,27], [27,25], [27,26], [27,27], [50,50], [50,51], [51,50], [52,53], [53,52], [53,53]]; // "Glider" collides with "Beacon" and explodes
-seed = [[50,40], [50,44], [50,45], [50,46], [51,39], [51,40], [52,45]]; // "Diehard"
+//seed = [[50,40], [50,44], [50,45], [50,46], [51,39], [51,40], [52,45]]; // "Diehard"
 //seed = [[40,40], [42,40], [42,41], [44,42], [44,43], [44,44], [46,43], [46,44], [46,45], [47,44]]; // Infinite growth
 //seed = [[49,49], [50,48], [50,49], [50,50], [51,50]]; // "R-pentomino"
-//seed = [[47,51], [48,51], [48,53], [49,51], [49,52], [50,48], [51,49], [51,50], [52,48], [52,49]]; // "Two-glider mess"
+seed = [[47,51], [48,51], [48,53], [49,51], [49,52], [50,48], [51,49], [51,50], [52,48], [52,49]]; // "Two-glider mess"
 //seed = [[47,50], [48,50], [49,50], [50,50], [51,50], [52,50], [53,50]]; // Becomes a "Honey farm"
 //seed = [[,], [,], [,], [,], [,], [,]]; // Template
 
@@ -52,14 +65,20 @@ seed = [[50,40], [50,44], [50,45], [50,46], [51,39], [51,40], [52,45]]; // "Dieh
  * @param {number} pixelsPerSide The pixel height of each cell on the gameBoard
  * @return gameBoard Handle for the internal 2d array that handles live and dead cells
  */
-function setUpGameBoard(boardDim, pixelsPerSide) {
-	// Create variables for the drawing space
+function setUpGameBoard() {
+	// Get audio ready
+	audio = new Audio("SmoothMcGroove_Zelda_DekuPalace.mp3");
+	// Connect vars to the DOM
 	htmlCanvasElement = document.getElementById("board");
 	drawSpace = htmlCanvasElement.getContext("2d");
-	document.getElementById("speed").innerHTML = speed;
+	// Give user play/pause functionality (space bar and canvas click)
+	document.onkeydown = function(key) { if (key.keyCode == 32) playPause(); };
+	htmlCanvasElement.addEventListener("click", playPause);
+	// Set play speed
 	delay = 1000 - (speed*10);
+	document.getElementById("speed").innerHTML = speed;
 	// Set up the internal 2d array and initialize it 
-	boardDim += 4; // Add extra space around board edges
+	//gameBoard = new Array(boardDim).fill(new Array(boardDim).fill(0));
 	gameBoard = new Array(boardDim);
 	for(i=0; i<boardDim; i++) {
 		gameBoard[i] = new Array(boardDim);
@@ -72,42 +91,55 @@ function setUpGameBoard(boardDim, pixelsPerSide) {
 	}
 	// Draw the seed
 	for(i=0; i<seed.length; i++) {
-		x = (seed[i][0]-2) * pixelsPerSide;
-		y = (seed[i][1]-2) * pixelsPerSide;
+		x = (seed[i][0]-5) * pixelsPerSide;
+		y = (seed[i][1]-5) * pixelsPerSide;
 		drawSpace.fillRect(x,y,pixelsPerSide,pixelsPerSide);
 	}
 	// Change the button behaviour and return the board
 	setUpButton = document.getElementById("setUp");
 	setUpButton.onclick = transitionBoard;
 	setUpButton.innerHTML = "Step";
-	return gameBoard;
 }
 
+/**
+ * Handle the "Start" button.
+ */
 function playButton() {
-	var audio = new Audio("SmoothMcGroove_Zelda_DekuPalace.mp3");
 	audio.play();
 	game = setInterval(function() {
-		transitionBoard(gameBoard,boardDim,pixelsPerSide,drawSpace)
+		transitionBoard()
 		}, delay);
+	isPaused = false;
+}
+
+/**
+ * Handle the "Pause" button.
+ */
+function pauseButton() {
+	audio.pause(); 
+	clearTimeout(game);
+	isPaused = true;
+}
+
+/**
+ * Used by event listeners to either play or pause.
+ */
+function playPause() {
+	if (isPaused)
+		playButton();
+	else
+		pauseButton();
 }
 
 /**
  * This is just a wrapper for two functions that are commonly called one after the other. In 
  * a later version, the two functions may be combined, or the first might call the second.
  */
-function transitionBoard(gameBoard, boardDim, pixelsPerSide, drawSpace) {
-	boardDim += 4;
-	transformBoard(gameBoard, boardDim);
-	updateCanvas(gameBoard, boardDim, pixelsPerSide, drawSpace);
+function transitionBoard() {
+	transformBoard();
+	updateCanvas();
 }
 
-/**
- * This is a function overload that makes the 'Step' button work.
- */
-function transitionBoard() {
-	transformBoard(gameBoard, boardDim);
-	updateCanvas(gameBoard, boardDim, pixelsPerSide, drawSpace);
-}
 
 /**
  * Checks each cell in gameBoard and possibly changes it, using the 4 rules of the Game of 
@@ -122,10 +154,10 @@ function transitionBoard() {
  * @return gameBoard This is returned for formality; the gameBoard that is passed in actually
  *             changes
  */
-function transformBoard(gameBoard, boardDim) {
+function transformBoard() {
 	for(col=0; col<boardDim; col++) {
 		for(row=0; row<boardDim; row++) {
-			// Count the live neighbors
+			/// Count the live neighbors ///
 			liveNeighbors = 0;
 			for(i=col-1; i<col+2; i++) {
 				for(j=row-1; j<row+2; j++) {
@@ -141,23 +173,22 @@ function transformBoard(gameBoard, boardDim) {
 				}
 			}
 			currentCell = gameBoard[col][row];
-			// Rule 1
+			/// Rule 1 ///
 			if((currentCell==1||currentCell==-1) && liveNeighbors<2) {
 				gameBoard[col][row] = -1;
 			}
 			// Rule 2 needs no transformation
-			// Rule 3
+			/// Rule 3 ///
 			if((currentCell==1||currentCell==-1) && liveNeighbors>3) {
 				gameBoard[col][row] = -1;
 			}
-			// Rule 4
+			/// Rule 4 ///
 			if((currentCell==0||currentCell==2) && liveNeighbors==3) {
 				gameBoard[col][row] = 2;
 			}
 		}
 	}
 	generation += 1;
-	return gameBoard;
 }
 
 /**
@@ -170,27 +201,21 @@ function transformBoard(gameBoard, boardDim) {
  * @param {number} pixelsPerSide The pixel height of each cell on the gameBoard
  * @param drawSpace Html DOM handle for the drawing area
  */
-function updateCanvas(gameBoard, boardDim, pixelsPerSide, drawSpace) {
-	for(col=0; col<boardDim; col++) {
-		for(row=0; row<boardDim; row++) {
+function updateCanvas() {
+	for(col=5; col<boardDim-5; col++) {
+		for(row=5; row<boardDim-5; row++) {
+			var x = (col-5) * pixelsPerSide;
+			var y = (row-5) * pixelsPerSide;
 			if(gameBoard[col][row]==2) {
-				// Draw a black square if on the board, then set the value to 1
-				if(col>1 && col<boardDim-4 && row>1 && row<boardDim-4) {
-					x = (col-2) * pixelsPerSide;
-					y = (row-2) * pixelsPerSide;
-					drawSpace.fillRect(x,y,pixelsPerSide,pixelsPerSide);
-				}
+				// Draw a black square on the board, then set the value to 1
+				drawSpace.fillRect(x,y,pixelsPerSide,pixelsPerSide);
 				gameBoard[col][row] = 1;
 			}
 			if(gameBoard[col][row]==-1) {
-				// Draw a white square if on the board, then set the value to 0
-				if(col>1 && col<boardDim-4 && row>1 && row<boardDim-4) {
-					x = (col-2) * pixelsPerSide;
-					y = (row-2) * pixelsPerSide;
-					drawSpace.fillStyle = "#FFFFFF";
-					drawSpace.fillRect(x,y,pixelsPerSide,pixelsPerSide);
-					drawSpace.fillStyle = "#000000";
-				}
+				// Draw a white square on the board, then set the value to 0
+				drawSpace.fillStyle = "#FFFFFF";
+				drawSpace.fillRect(x,y,pixelsPerSide,pixelsPerSide);
+				drawSpace.fillStyle = "#000000";
 				gameBoard[col][row] = 0;
 			}
 			// else gameBoard[col][row] == 0 or 1, so take no action
